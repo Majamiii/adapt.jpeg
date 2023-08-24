a79e851fbc83fcbf18aa96888cb527503e4da682
Maja Milovic <maja.milovic05@gmail.com>
2023-08-24 12:02:05 +0200

    c c++ interop

---
+++ majapeg.cpp
@@ -0,0 +1,8 @@
+#include "majapeg.h"
+
+#include <iostream>
+
+int majapeg(int a) {
+    std::cout<<"Hello from majapeg"<<std::endl;
+    return 9;
+}
---
+++ majapeg.h
@@ -0,0 +1,15 @@
+#ifndef MAJAPEG_H
+#define MAJAPEG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int majapeg(int a);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // MAJAPEG_H
