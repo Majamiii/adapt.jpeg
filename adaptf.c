f4ba9c23238e8700707bb9037959d5a6285508c6
Maja Milovic <maja.milovic05@gmail.com>
2023-08-30 18:41:21 +0200

    na pametan nacin skalirani koeficijenti KONACNO

--- adaptf.c
+++ adaptf.c
@@ -50,62 +50,64 @@ int pixel_height(int widthp)
 
 int one_block(int nofpixels)
 {   
-    FILE *fptr;
-    fptr = fopen("./modicoefs.txt","a");      //make a file for storing modified coefficients
-
     FILE *fp;
     fp = fopen("./dctcoefs.txt", "r");
 
     
-    int n;
-    float ogcoefs[nofpixels];
-    /*int i = 0;
-    int j=0;
-    int k=0;*/
-    float sum = 0;
+   // int n, j, k;
+    int ogcoefs[nofpixels];
+    int newcoefs[nofpixels];
+    
+    int sum = 0;
     //changing one block of DCT coefficients
 
     for (int i = 0; i < 64; i++) {          //change 64 later to DCTSIZE2, do this for every pixel
-        int x = fscanf(fp, "%f\n", &ogcoefs[i]); //store info in ogcoefs array
-
-        int kurac = 14;     //igor influence
-
-        for(int zbir = 0; zbir<14; zbir++) {
-
-                for (int k=0, j=zbir; j<k; k++, j--) {
-                    ogcoefs[k*8+j] *= (zbir+1)/(kurac+1);
-                }
-        }
-
-        sum += ogcoefs[i];
-        fprintf(fptr, "%f\n", ogcoefs[i]);
-
+        int x = fscanf(fp, "%i\n", &ogcoefs[i]); //store info in ogcoefs array
     }
 
-    fclose(fptr);
+  
+    int brojilac[63];
+    int broj;
+    int k=0;
+    int j=0;
+    for (int i=0; i<64; i++, j++) {
+
+        broj = j+1;
+        brojilac[i] = broj;
+
+        if(j==k+7) {
+            j=j-7;
+            k = k+1;
+        }
+
+        // printf("%i ", broj);
+
+        newcoefs[i] = ogcoefs[i] * broj / 15;
+
+        //     printf("%i \n", newcoefs[i]);
+        
+        if (newcoefs[i] < 0) {
+            sum -= newcoefs[i];
+        }
+        else {
+            sum += newcoefs[i];
+        }
+    }
+
     fclose(fp);
 
-    float percent;
+    int percent;
     percent = sum / 43.69;
 
-    int integer;
-    integer = (int)percent;
+    printf("%i \n", percent);
 
-    return integer;
+    return percent;
 }
 
 int block()
 {
     int pixels = num_pixels();
-    //printf("%d\n", heightp);
-    FILE * fptr;
-    fptr = fopen("./modicoefs.txt","w");
-    fclose(fptr);
-    
     int percent = one_block(pixels);
-
-    //printf("%i ", percent);
-  
     
     return percent;
 }
